3cb1e48a730cc91c6faf06fd8ef13f832f06820b
nlhsueh <nlhsueh@fcu.edu.tw>
2015-12-20 11:34:35 +0800

    mediator template code

--- src/mediator/template/MediatorTemplate.java
+++ src/mediator/template/MediatorTemplate.java
@@ -4,8 +4,12 @@ public class MediatorTemplate {
 
 	public static void main(String[] args) {
 		IMediator med = new Mediator();
+
+		// 生成 colleague 時，設定其 Mediator
 		Colleague1 c1 = new Colleague1(med);
 		Colleague2 c2 = new Colleague2(med);
+
+		// 送訊息給 c1, c2
 		c1.m1();
 		c2.m2();
 	}
@@ -25,6 +29,8 @@ class Mediator implements IMediator {
 	Colleague1 c1;
 	Colleague2 c2;
 
+	// 雖然 m1 是給 Colleague1 的訊息，但會轉給 mediator
+	// mediator 來決定會與哪些其他 Colleague 互動
 	public void m1() {
 		System.out.println("Mediator m1");
 		c2.op2();
@@ -35,6 +41,7 @@ class Mediator implements IMediator {
 		c1.op1();
 	}
 
+	// Mediator 必須識得每一個 Colleague
 	public void registerColleague1(Colleague1 c) {
 		this.c1 = c;
 	}
